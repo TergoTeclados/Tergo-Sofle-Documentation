643d268952da72fc47cf794145ac14d51a011a5e
victor <victor.s.camargo@gmail.com>
2025-09-03 22:04:47 -0300

    Update linguagem default para pt-BR

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -32,8 +32,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR'],
   },
 
   presets: [
