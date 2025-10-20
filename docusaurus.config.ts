3754a4e41ffa8baeaa4f4e475c8e8b7c4a14c679
VictorSCamargo <victor.s.camargo@gmail.com>
2025-10-20 11:01:47 -0300

    add Algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,6 +72,41 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'TUZ2MWH83T',
+
+      // Public API key: it is safe to commit it
+      apiKey: '84fb31d203f8198c8a614fef3e0345b7',
+
+      indexName: 'Tergo Sofle docs',
+
+      // Optional: see doc section below
+      // contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      // searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      // insights: false,
+
+      // Optional: whether you want to use the new Ask AI feature (undefined by default)
+      // askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
+
+      //... other Algolia params
+    },
     colorMode: {
       respectPrefersColorScheme: true,
       defaultMode: 'dark',
